f043dafaa36c2013586e2455462b30fd543948aa
Nicolas Lefebvre <nlefebvr@ualberta.ca>
2019-01-20 15:34:19 -0700

    added mute button

--- contentScript.js
+++ contentScript.js
@@ -3,10 +3,22 @@ const setText = () => {
     if (micButton.length > 0) {
         micButton[0].innerText = "microphone";
     } else {
-        console.log("button unavailable");
+        console.log("mic button unavailable");
+        setTimeout(setText, 5000);
+    }
+};
+
+
+const setVolume = () => {
+    const volumeButton = document.getElementsByClassName("volume player-button");
+    if (volumeButton.length > 0) {
+        micButton[0].innerText = "unmute";
+    } else {
+        console.log("volume button unavailable");
         setTimeout(setText, 5000);
     }
 };
 
 setText();
+setVolume();
 
