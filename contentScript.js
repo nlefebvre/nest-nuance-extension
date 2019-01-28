78131ca2ea5295664a3357e77f04b4911f6f76bd
Nicolas Lefebvre <nlefebvr@ualberta.ca>
2019-01-27 17:59:15 -0700

    unmute on click

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,37 @@
+const getElement = (classNameString) => document.getElementsByClassName(classNameString)[0];
+const getVolumeButton = (isMuted) => getElement(`volume player-button${isMuted ? " is-muted" : ""}`);
+// const getVolumeButton = (isMuted) => getElement(`volume player-button${isMuted ? " is-muted" :""} player-button`);
+const getMicButton = () => getElement("player-button talkback");
+
+const toggleMute = () => {
+    const volumeButton = getVolumeButton(true);
+    if (volumeButton) {
+        volumeButton.click();
+    }
+}
+
+
 const setText = () => {
-    const micButton = document.getElementsByClassName("player-button talkback");
-    if (micButton.length > 0) {
-        micButton[0].innerText = "microphone";
+    const micButton = getMicButton()
+    if (micButton) {
+        micButton.innerText = "microphone";
+        micButton.onclick = toggleMute;
     } else {
         console.log("mic button unavailable");
         setTimeout(setText, 5000);
     }
 };
 
-
 const setVolume = () => {
-    const volumeButton = document.getElementsByClassName("volume player-button");
-    if (volumeButton.length > 0) {
-        micButton[0].innerText = "unmute";
+    const volumeButton = getVolumeButton();
+    if (volumeButton) {
+        volumeButton.innerText = "unmute";
     } else {
         console.log("volume button unavailable");
-        setTimeout(setText, 5000);
+        setTimeout(setVolume, 5000);
     }
 };
 
 setText();
-setVolume();
+// setVolume();
 
